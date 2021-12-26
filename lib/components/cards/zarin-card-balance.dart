b0feaab1b9d7e27ae8dfbf7779e76c2ad46902a5
Amirhossein Khani <eamirho3einkhani@gmail.com>
2021-12-26 10:52:11 +0330

    fix: fix zarin-card-balance skeleton off

--- lib/components/cards/zarin-card-balance.dart
+++ lib/components/cards/zarin-card-balance.dart
@@ -27,105 +27,161 @@ class ZarinCardBalance extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         color: style.backgroundColor,
       ),
-      child: Skeleton(
-        setting: setting,
-        enabled: isSkeleton,
-        child: CustomCardTypeOne(
-          header: Padding(
-            // title
-            padding: EdgeInsets.only(bottom: 24),
-            child: isSkeleton
-                ? Container(
-                    width: 104,
-                    height: 12,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(2),
-                      color: setting.color,
-                    ),
-                  )
-                : Text(
-                    title,
-                    textAlign: TextAlign.start,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2
-                        .copyWith(color: style.secondaryTextColor),
-                  ),
+      child: isSkeleton ? _buildSkeleton(context) : _buildView(context),
+    );
+  }
+
+  _buildSkeleton(BuildContext context) {
+    return Skeleton(
+      setting: setting,
+      enabled: isSkeleton,
+      child: CustomCardTypeOne(
+        header: Padding(
+          // title
+          padding: EdgeInsets.only(bottom: 24),
+          child: Text(
+            title,
+            textAlign: TextAlign.start,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2
+                .copyWith(color: style.secondaryTextColor),
           ),
-          content: Row(
-            // amount
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+        content: Row(
+          // amount
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 176,
+              height: 24,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: setting.color,
+              ),
+            ),
+            Container(
+              width: 40,
+              height: 12,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2),
+                color: setting.color,
+              ),
+            ),
+          ],
+        ),
+        buttons: Padding(
+          padding: const EdgeInsets.only(top: 24),
+          child: Row(
             children: [
-              isSkeleton
-                  ? Container(
-                      width: 176,
-                      height: 24,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: setting.color,
+              rightButton != null
+                  ? Expanded(
+                      child: CustomButton(
+                        title: rightButton.text,
+                        size: ButtonSize.large,
+                        onClick: rightButton.onClick,
+                        style: CustomButtonTheme(
+                          backgroundColor: rightButton.style.backgroundColor,
+                          foregroundColor: rightButton.style.foregroundColor,
+                          disabledColor: rightButton.style.disabledColor,
+                        ),
+                        rightIcon: rightButton.icon.icon,
                       ),
                     )
-                  : Text(
-                      amount,
-                      textAlign: TextAlign.start,
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-              isSkeleton
-                  ? Container(
-                      width: 40,
-                      height: 12,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(2),
-                        color: setting.color,
+                  : SizedBox(),
+              SizedBox(
+                width: leftButton != null ? 12 : 0,
+              ),
+              leftButton != null
+                  ? Expanded(
+                      child: CustomButton(
+                        title: leftButton.text,
+                        size: ButtonSize.large,
+                        onClick: rightButton.onClick,
+                        style: CustomButtonTheme(
+                          backgroundColor: leftButton.style.backgroundColor,
+                          foregroundColor: leftButton.style.foregroundColor,
+                          disabledColor: leftButton.style.disabledColor,
+                        ),
+                        rightIcon: leftButton.icon.icon,
                       ),
                     )
-                  : Text(
-                      ' ' + 'ریال',
-                      textAlign: TextAlign.end,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
+                  : SizedBox()
             ],
           ),
-          buttons: Padding(
-            padding: const EdgeInsets.only(top: 24),
-            child: Row(
-              children: [
-                rightButton != null
-                    ? Expanded(
-                        child: CustomButton(
-                          title: rightButton.text,
-                          size: ButtonSize.large,
-                          onClick: rightButton.onClick,
-                          style: CustomButtonTheme(
-                            backgroundColor: rightButton.style.backgroundColor,
-                            foregroundColor: rightButton.style.foregroundColor,
-                            disabledColor: rightButton.style.disabledColor,
-                          ),
-                          rightIcon: rightButton.icon.icon,
-                        ),
-                      )
-                    : SizedBox(),
-                SizedBox(
-                  width: leftButton != null ? 12 : 0,
-                ),
-                leftButton != null
-                    ? Expanded(
-                        child: CustomButton(
-                          title: leftButton.text,
-                          size: ButtonSize.large,
-                          onClick: rightButton.onClick,
-                          style: CustomButtonTheme(
-                            backgroundColor: leftButton.style.backgroundColor,
-                            foregroundColor: leftButton.style.foregroundColor,
-                            disabledColor: leftButton.style.disabledColor,
-                          ),
-                          rightIcon: leftButton.icon.icon,
-                        ),
-                      )
-                    : SizedBox()
-              ],
-            ),
+        ),
+      ),
+    );
+  }
+
+  _buildView(BuildContext context) {
+    return CustomCardTypeOne(
+      header: Padding(
+        // title
+        padding: EdgeInsets.only(bottom: 24),
+        child: Text(
+          title,
+          textAlign: TextAlign.start,
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2
+              .copyWith(color: style.secondaryTextColor),
+        ),
+      ),
+      content: Row(
+        // amount
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            amount,
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.headline1,
           ),
+          Text(
+            ' ' + 'ریال',
+            textAlign: TextAlign.end,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
+      ),
+      buttons: Padding(
+        padding: const EdgeInsets.only(top: 24),
+        child: Row(
+          children: [
+            rightButton != null
+                ? Expanded(
+                    child: CustomButton(
+                      title: rightButton.text,
+                      size: ButtonSize.large,
+                      onClick: rightButton.onClick,
+                      style: CustomButtonTheme(
+                        backgroundColor: rightButton.style.backgroundColor,
+                        foregroundColor: rightButton.style.foregroundColor,
+                        disabledColor: rightButton.style.disabledColor,
+                      ),
+                      rightIcon: rightButton.icon.icon,
+                    ),
+                  )
+                : SizedBox(),
+            SizedBox(
+              width: leftButton != null ? 12 : 0,
+            ),
+            leftButton != null
+                ? Expanded(
+                    child: CustomButton(
+                      title: leftButton.text,
+                      size: ButtonSize.large,
+                      onClick: rightButton.onClick,
+                      style: CustomButtonTheme(
+                        backgroundColor: leftButton.style.backgroundColor,
+                        foregroundColor: leftButton.style.foregroundColor,
+                        disabledColor: leftButton.style.disabledColor,
+                      ),
+                      rightIcon: leftButton.icon.icon,
+                    ),
+                  )
+                : SizedBox()
+          ],
         ),
       ),
     );
