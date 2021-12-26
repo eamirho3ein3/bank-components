99a1b54a928c39127469330037d66558913e62b3
Amirhossein Khani <eamirho3einkhani@gmail.com>
2021-12-26 13:39:05 +0330

    feat: add shimmer to transaction-itme

--- lib/components/transaction-item.dart
+++ lib/components/transaction-item.dart
@@ -6,75 +6,94 @@ class TransactionItem extends StatelessWidget {
   final String subtitle;
   final String price;
   final TransactionItemTheme style;
-  TransactionItem(
-      {@required this.icon,
-      @required this.title,
-      @required this.subtitle,
-      @required this.style,
-      @required this.price});
+  final SkeletonSetting setting;
+  final bool isSkeleton;
+
+  TransactionItem({
+    @required this.icon,
+    @required this.title,
+    @required this.subtitle,
+    @required this.style,
+    @required this.price,
+    @required this.isSkeleton,
+    @required this.setting,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: style.backgroundColor,
       padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      child: Row(
-        children: [
-          // logo
-          CircleAvatar(
-            backgroundColor: icon.backgroundColor,
-            radius: 24,
-            child: Icon(icon.icon, color: icon.iconColor),
-          ),
-          SizedBox(
-            width: 12,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // title
-                    Text(title, style: Theme.of(context).textTheme.subtitle1),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    // price
-                    Expanded(
-                      child: Text.rich(
-                        TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: price,
-                                style: Theme.of(context).textTheme.subtitle1),
-                            TextSpan(
-                              text: ' ',
-                            ),
-                            TextSpan(
-                                text: 'ریال',
-                                style: Theme.of(context).textTheme.caption),
-                          ],
-                        ),
-                        textAlign: TextAlign.left,
+      child: isSkeleton ? _buildShimmer(context) : _buildView(context),
+    );
+  }
+
+  _buildShimmer(BuildContext context) {
+    return Skeleton(
+      setting: setting,
+      enabled: true,
+      child: _buildView(context),
+    );
+  }
+
+  _buildView(BuildContext context) {
+    return Row(
+      children: [
+        // logo
+        CircleAvatar(
+          backgroundColor: icon.backgroundColor,
+          radius: 24,
+          child: Icon(icon.icon, color: icon.iconColor),
+        ),
+        SizedBox(
+          width: 12,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // title
+                  Text(title, style: Theme.of(context).textTheme.subtitle1),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  // price
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: price,
+                              style: Theme.of(context).textTheme.subtitle1),
+                          TextSpan(
+                            text: ' ',
+                          ),
+                          TextSpan(
+                              text: 'ریال',
+                              style: Theme.of(context).textTheme.caption),
+                        ],
                       ),
+                      textAlign: TextAlign.left,
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                // subtitle
-                Text(subtitle,
-                    style: Theme.of(context)
-                        .textTheme
-                        .caption
-                        .copyWith(color: style.subtitleColor)),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              // subtitle
+              Text(subtitle,
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption
+                      .copyWith(color: style.subtitleColor)),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
