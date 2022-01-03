8c91c85300c822840d73bfe70a0a06a4879e9440
Amirhossein Khani <eamirho3einkhani@gmail.com>
2022-01-03 13:43:14 +0330

    chore: make custom-segmented component change index inside of it self

--- lib/components/segmented.dart
+++ lib/components/segmented.dart
@@ -1,27 +1,41 @@
 part of bank_components;
 
-class CustomSegmented extends StatelessWidget {
+class CustomSegmented extends StatefulWidget {
   final CustomSegmentedTheme style;
-  final int selectedIndex;
+  final int initIndex;
   final Function(int) onTabClick;
   final List<String> tabs;
   CustomSegmented(
       {@required this.style,
-      @required this.selectedIndex,
+      this.initIndex,
       this.onTabClick,
       @required this.tabs});
 
   @override
+  _CustomSegmentedState createState() => _CustomSegmentedState();
+}
+
+class _CustomSegmentedState extends State<CustomSegmented> {
+  var _selectedIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedIndex = widget.initIndex ?? -1;
+  }
+
+  @override
   Widget build(BuildContext context) {
     List<Widget> _children = [];
-    for (var i = 0; i < tabs.length; i++) {
+    for (var i = 0; i < widget.tabs.length; i++) {
       _children.add(_buildTabItem(context, i));
     }
 
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
-        color: style.backgroundColor,
+        color: widget.style.backgroundColor,
       ),
       child: Row(
         mainAxisSize: MainAxisSize.max,
@@ -36,20 +50,25 @@ class CustomSegmented extends StatelessWidget {
         padding: EdgeInsets.all(4),
         child: InkWell(
           onTap: () {
-            onTabClick(index);
+            setState(() {
+              _selectedIndex = index;
+            });
+            if (widget.onTabClick != null) {
+              widget.onTabClick(index);
+            }
           },
           child: AnimatedContainer(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(4),
-              color: index == selectedIndex
-                  ? style.selectedBackgroundColor
-                  : style.backgroundColor,
+              color: index == _selectedIndex
+                  ? widget.style.selectedBackgroundColor
+                  : widget.style.backgroundColor,
             ),
             padding: EdgeInsets.symmetric(vertical: 8),
             curve: Curves.easeOut,
             duration: Duration(milliseconds: 300),
             child: Text(
-              tabs[index],
+              widget.tabs[index],
               textAlign: TextAlign.center,
               style: Theme.of(context)
                   .textTheme
