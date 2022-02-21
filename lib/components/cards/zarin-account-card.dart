50cb7d61c1b03fd3832432dbae6675daa44e2086
Amirhossein Khani <eamirho3einkhani@gmail.com>
2022-02-21 12:16:19 +0330

    chore: add skeleton to zarin-account-card component

--- lib/components/cards/zarin-account-card.dart
+++ lib/components/cards/zarin-account-card.dart
@@ -6,6 +6,8 @@ class ZarinAccountCard extends StatelessWidget {
   final String amount;
   final Function onMoreClick;
   final CustomIconTheme icon;
+  final bool isSkeleton;
+  final SkeletonSetting setting;
 
   ZarinAccountCard({
     @required this.style,
@@ -13,29 +15,38 @@ class ZarinAccountCard extends StatelessWidget {
     @required this.amount,
     @required this.onMoreClick,
     @required this.icon,
+    @required this.isSkeleton,
+    @required this.setting,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: style.backgroundColor,
       constraints:
           BoxConstraints(maxWidth: MediaQuery.of(context).size.width * (0.56)),
+      child: isSkeleton ? _buildSkeleton(context) : _buildView(context),
+    );
+  }
+
+  _buildSkeleton(BuildContext context) {
+    return Skeleton(
+      setting: setting,
+      enabled: isSkeleton,
       child: CustomCardTypeTwo(
         header: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             // account icon
-            CustomAvatar(
+            CircleAvatar(
               radius: 24,
-              icon: icon.icon,
-              iconColor: icon.iconColor,
-              backgroundColor: icon.backgroundColor,
+              backgroundColor: setting.color,
             ),
 
             // more button
             InkWell(
-              onTap: onMoreClick,
+              onTap: () {},
               child: Icon(Icons.more_horiz_rounded),
             ),
           ],
@@ -43,28 +54,75 @@ class ZarinAccountCard extends StatelessWidget {
         title: Padding(
           //account title
           padding: const EdgeInsets.only(bottom: 8, top: 28),
-          child: Text(
-            title,
-            textAlign: TextAlign.end,
-            style: Theme.of(context).textTheme.bodyText2,
+          child: Container(
+            width: 120,
+            height: 12,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2),
+              color: setting.color,
+            ),
           ),
         ),
         subtitle:
             // amount
-            RichText(
-          textAlign: TextAlign.end,
-          text: TextSpan(
-              text: amount,
-              style: Theme.of(context).textTheme.subtitle1,
-              children: <TextSpan>[
-                TextSpan(
-                  text: ' ' + 'ریال',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ]),
+            Container(
+          width: 64,
+          height: 12,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(2),
+            color: setting.color,
+          ),
         ),
-        style: null,
+        style: style,
       ),
+    );
+  }
+
+  _buildView(BuildContext context) {
+    return CustomCardTypeTwo(
+      header: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // account icon
+          CustomAvatar(
+            radius: 24,
+            icon: icon.icon,
+            iconColor: icon.iconColor,
+            backgroundColor: icon.backgroundColor,
+          ),
+
+          // more button
+          InkWell(
+            onTap: onMoreClick,
+            child: Icon(Icons.more_horiz_rounded),
+          ),
+        ],
+      ),
+      title: Padding(
+        //account title
+        padding: const EdgeInsets.only(bottom: 8, top: 28),
+        child: Text(
+          title,
+          textAlign: TextAlign.end,
+          style: Theme.of(context).textTheme.bodyText2,
+        ),
+      ),
+      subtitle:
+          // amount
+          RichText(
+        textAlign: TextAlign.end,
+        text: TextSpan(
+            text: amount,
+            style: Theme.of(context).textTheme.subtitle1,
+            children: <TextSpan>[
+              TextSpan(
+                text: ' ' + 'ریال',
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ]),
+      ),
+      style: style,
     );
   }
 }
