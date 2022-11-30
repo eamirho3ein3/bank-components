f12caafc732f012fdb3e3059f85e280791e53f47
Amirhossein Khani <eamirho3einkhani@gmail.com>
2022-11-30 17:13:29 +0330

    chore: add card tap to ZarinAccountBalance

--- lib/components/cards/zarin-account-balance.dart
+++ lib/components/cards/zarin-account-balance.dart
@@ -6,6 +6,7 @@ class ZarinAccountBalance extends StatelessWidget {
   final String title;
   final String amount;
   final String priceUnit;
+  final Function() cardTap;
 
   ZarinAccountBalance({
     @required this.button,
@@ -13,59 +14,63 @@ class ZarinAccountBalance extends StatelessWidget {
     @required this.title,
     @required this.amount,
     @required this.priceUnit,
+    this.cardTap,
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: style.backgroundColor,
-      ),
-      child: CustomCardTypeOne(
-        header: Padding(
-          // title
-          padding: EdgeInsets.only(bottom: 24),
-          child: Text(
-            title,
-            textAlign: TextAlign.start,
-            style: Theme.of(context)
-                .textTheme
-                .bodyText2
-                .copyWith(color: style.secondaryTextColor),
-          ),
+    return GestureDetector(
+      onTap: cardTap,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: style.backgroundColor,
         ),
-        content: Row(
-          // amount
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              amount,
+        child: CustomCardTypeOne(
+          header: Padding(
+            // title
+            padding: EdgeInsets.only(bottom: 24),
+            child: Text(
+              title,
               textAlign: TextAlign.start,
               style: Theme.of(context)
                   .textTheme
-                  .headline1
+                  .bodyText2
                   .copyWith(color: style.secondaryTextColor),
             ),
-            Text(
-              ' ' + priceUnit,
-              textAlign: TextAlign.end,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText1
-                  .copyWith(color: style.secondaryTextColor),
+          ),
+          content: Row(
+            // amount
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                amount,
+                textAlign: TextAlign.start,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline1
+                    .copyWith(color: style.secondaryTextColor),
+              ),
+              Text(
+                ' ' + priceUnit,
+                textAlign: TextAlign.end,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(color: style.secondaryTextColor),
+              ),
+            ],
+          ),
+          buttons: Container(
+            padding: EdgeInsets.only(top: 24),
+            width: double.infinity,
+            child: CustomButton(
+              title: button.text,
+              size: ButtonSize.large,
+              onClick: button.onClick,
+              style: button.style,
+              rightIcon: Icons.pie_chart_outline_sharp,
             ),
-          ],
-        ),
-        buttons: Container(
-          padding: EdgeInsets.only(top: 24),
-          width: double.infinity,
-          child: CustomButton(
-            title: button.text,
-            size: ButtonSize.large,
-            onClick: button.onClick,
-            style: button.style,
-            rightIcon: Icons.pie_chart_outline_sharp,
           ),
         ),
       ),
