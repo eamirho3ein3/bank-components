50d6153f7177dcc97d5e6fb9006875bcac86a8ea
Amirhossein Khani <eamirho3einkhani@gmail.com>
2023-01-31 16:12:36 +0330

    chore: update mainAppBar

--- lib/components/appbars/main-appbar.dart
+++ lib/components/appbars/main-appbar.dart
@@ -1,34 +1,26 @@
 part of bank_components;
 
-class MainAppBar extends StatelessWidget {
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final List<CustomIconButton> actions;
 
   MainAppBar({@required this.title, @required this.actions});
+  static final _appBar = AppBar();
+
+  @override
+  Size get preferredSize => _appBar.preferredSize;
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> _children = [];
-    var index = 1;
-    for (var item in actions) {
-      _children.add(item);
-      if (index != actions.length) {
-        _children.add(SizedBox(
-          width: 12,
-        ));
-      }
-      index++;
-    }
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          style: Theme.of(context).textTheme.headline2,
-        ),
-        Row(
-          children: _children,
-        ),
-      ],
+    return AppBar(
+      elevation: 0,
+      centerTitle: false,
+      actions: actions,
+      backgroundColor: Colors.transparent,
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.headline2,
+      ),
     );
   }
 }
